965589fb0c2709471312d3b438d9d15d572fd280
daxas-boop <daxas.swd@hotmail.com>
2020-10-07 00:12:45 -0300

    Adds router for the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NavBar from './components/Nav'
+import Shop from './components/Shop'
+import Home from './components/Home'
+import Cart from './components/ShoppingCart'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/shop" component={Shop} />
+          <Route path="/shop-cart" component={Cart} />
+        </Switch>
+      </Router>
     </div>
   );
 }
