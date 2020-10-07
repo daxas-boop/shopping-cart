acb9df487872083308e2fb4012a783fb0c8160ac
daxas-boop <daxas.swd@hotmail.com>
2020-10-07 00:13:27 -0300

    Adds Nav.js component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Header = styled.header`
+  width:100%;
+  background-color:#D5AF9A;
+  height:64px;
+  display:flex;
+  align-items:center;
+  opacity:0.8;
+`
+
+const Nav = styled.nav `
+  flex-grow:1;
+  text-align:center;
+`
+
+const NavLink = styled(Link) `
+  text-decoration:none;
+  color:white;
+  margin:0 15px;
+  padding:5px;
+  font-size:24px;
+  font-family:Roboto;
+  &:hover{
+    color:#AD6B48;
+  }
+`
+
+const CartIcon = styled(FontAwesomeIcon) `
+  color:white;
+  margin-right:20px;
+  &:hover{
+    color:#AD6B48;
+    cursor:pointer;
+  }
+`
+
+const NavBar = () => {
+  return (
+    <Header>
+      <Nav>
+        <NavLink to='/'>
+          Home
+        </NavLink>
+        <NavLink to='shop'>
+          Shop
+        </NavLink>
+      </Nav>
+      <CartIcon icon={faShoppingCart} size='lg' ></CartIcon>
+    </Header>
+  );  
+}
+
+export default NavBar
