4c86f9faeed480d1031f5773e7dfb8e388a2221d
daxas-boop <daxas.swd@hotmail.com>
2020-10-09 21:41:02 -0300

    Updates

---
+++ precache-manifest.d6d853b680d853fafddcd219a64fa6a3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b8cea212103dc8e4168fba69315384",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "dac0c949064575f87d44",
+    "url": "/shopping-cart/static/js/2.09a7df14.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/shopping-cart/static/js/2.09a7df14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06698effbf722e84cd3",
+    "url": "/shopping-cart/static/js/main.86636f01.chunk.js"
+  },
+  {
+    "revision": "338f7be604fe84c0042f",
+    "url": "/shopping-cart/static/js/runtime-main.221af841.js"
+  },
+  {
+    "revision": "e714c7f7c9eae5e325e034906ff8bcd0",
+    "url": "/shopping-cart/static/media/Jordan-1.e714c7f7.jpg"
+  },
+  {
+    "revision": "28af589cc782613affa20b6bdd65971c",
+    "url": "/shopping-cart/static/media/Nike-Kobe-5-Protro.28af589c.jpg"
+  },
+  {
+    "revision": "90aae327814043e8cb8fc79645e74647",
+    "url": "/shopping-cart/static/media/TopstypeWomen.90aae327.jpg"
+  },
+  {
+    "revision": "f8f193a612235fdc33e1487ff6f735e2",
+    "url": "/shopping-cart/static/media/UnderArmorMen.f8f193a6.jpg"
+  },
+  {
+    "revision": "a99ea80c2b520c915a05d61f7987c95d",
+    "url": "/shopping-cart/static/media/Widget-1.a99ea80c.png"
+  },
+  {
+    "revision": "d26bfbd0faefdd0edfd6b1ff40d2ad19",
+    "url": "/shopping-cart/static/media/Widget-2.d26bfbd0.png"
+  },
+  {
+    "revision": "33e8a308f2507acc149c3192c7211433",
+    "url": "/shopping-cart/static/media/Widget-3.33e8a308.png"
+  },
+  {
+    "revision": "604326a67b8f7a748a1605e336ff64ad",
+    "url": "/shopping-cart/static/media/bottom_sweatpants.604326a6.jpg"
+  },
+  {
+    "revision": "874c62dc73c3a3f95de6ccc06548364a",
+    "url": "/shopping-cart/static/media/cloudfoam.874c62dc.jpg"
+  },
+  {
+    "revision": "11a187d1ed9331dcd41c25236879858b",
+    "url": "/shopping-cart/static/media/compression_shorts.11a187d1.jpg"
+  },
+  {
+    "revision": "adf001d611a7755eb8cf3627b086694f",
+    "url": "/shopping-cart/static/media/dockers.adf001d6.jpg"
+  },
+  {
+    "revision": "5d0963d045f0a52553d116cbbd685463",
+    "url": "/shopping-cart/static/media/grand_court.5d0963d0.jpg"
+  },
+  {
+    "revision": "111ac620f0335fe53bbcd32f70f2af81",
+    "url": "/shopping-cart/static/media/long_sleeve.111ac620.jpg"
+  },
+  {
+    "revision": "f67e5e52e0e8f045ea3787db2a30d1d6",
+    "url": "/shopping-cart/static/media/mainHome.f67e5e52.jpg"
+  },
+  {
+    "revision": "ce6cfea2dc24b767caaf961ca7034546",
+    "url": "/shopping-cart/static/media/summer_women.ce6cfea2.jpg"
+  }
+]);
